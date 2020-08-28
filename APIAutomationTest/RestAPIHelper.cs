08698149675c183682d17dbfa01ed1cb5f469ff0
GABRIEL GARCEZ DE CAMPOS <gabriel.campos@midway.com.br>
2020-08-28 17:44:44 -0300

    initial commit

---
+++ APIAutomationTest/RestAPIHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using RestSharp;
+
+namespace APIAutomationTest
+{
+    public static class RestAPIHelper
+    {
+        public static RestClient client;
+        public static RestRequest restRequest;
+        public static string baseUrl = "http://www.mydomain.com";
+
+        public static RestClient SetUrl(string endpoint)
+        {
+            var Url = Path.Combine(baseUrl, endpoint);
+            return client = new RestClient(Url);
+        }
+
+        public static RestRequest CreateRequest()
+        {
+            restRequest = new RestRequest(Method.GET);
+            restRequest.AddHeader("Accept", "application/json");
+            return restRequest;
+
+        }
+
+        public static IRestResponse GetResponse()
+        {
+
+            return client.Execute(restRequest);
+
+        }
+
+    }
+}
---
+++ APIAutomationTest/steps/APIApplication.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace APIAutomationTest.steps
+{
+    [Binding]
+    public sealed class APIApplication
+    {
+        [Given(@"i have an endpoint (.*)")]
+        public void GivenIHaveAnEndpointEndpoint(string endpoint)
+        {
+            RestAPIHelper.SetUrl(endpoint);
+        }
+
+        [When(@"I call get method of api")]
+        public void WhenICallGetMethodOfApi()
+        {
+            RestAPIHelper.CreateRequest();
+
+         }
+
+        [Then(@"I get api response in json format")]
+        public void ThenIGetApiResponseInJsonFormat()
+        {
+            var expected = "something";
+            var response = RestAPIHelper.GetResponse();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Assert.That(response.Content, Is.EqualTo(expected), "Error Message");
+            }
+        }
+
+    }
+}
